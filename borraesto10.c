4bf7babf0da0a11aa7c776188815a5b484b47bb2
Yomismismo <yop@AirdeYomismismo.home>
2024-01-24 23:26:17 +0100

    tal

--- borraesto10.c
+++ borraesto10.c
@@ -1,14 +1,13 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int main(void)
 {
-	char consigno = -32;
-	unsigned char sinsigno;
-	sinsigno = 0;
-	while (consigno < sinsigno)
-		sinsigno++;
-	if (consigno == sinsigno)
-		write(1, "S",1);
+	char consigno = -42; //214
+	unsigned char sinsigno = 214;
+	if (consigno == 214)
+		printf("-42 es lo mismo que 214 en char");
+
 	return (0);
 }
 
