97889d388c634c4cc87e878ebc5d87c501f1f1cf
Yomismismo <yop@AirdeYomismismo.home>
2024-02-06 13:49:19 +0100

    eo

--- ft_calloc.c
+++ ft_calloc.c
@@ -11,18 +11,18 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdint.h>
 void	*ft_calloc(size_t count, size_t size)
 {
 	size_t	total_size;
 	void	*pnt;
 
 //	if (count == 0 || size == 0 )
-//		return(malloc(0));
+//		return((void *)malloc(0));
 	if(count > 0 && size > (SIZE_MAX/count))
 		return(NULL);
 	total_size = count * size;
-	pnt = malloc(total_size);
+	pnt = (void *)malloc(total_size);
 	if (pnt)
 		ft_bzero(pnt, total_size);
 	return (pnt);
