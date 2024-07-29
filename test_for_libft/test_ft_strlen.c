d214c2e20b87c7366703dfd7667ab23faeaa9ccf
Anto <ant@onion.com>
2024-07-29 10:27:11 +0200

    limpieza de test

--- test_for_libft/test_ft_strlen.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-size_t	ft_strlen(const char *s);
-
-int main(void)
-{
-	char	*str = "holo Lalo";
-	printf("longitud esperado 9, resultado %ld\n",ft_strlen(str));
-}
--- test_putunbr_base.c
+++
@@ -1,8 +0,0 @@
-void	ft_putunbr_base(long unsigned int nbr, char *base);
-
-int main(void)
-{
-    long unsigned int n = 18446744073709551615UL;
-    ft_putunbr_base(n, "0123456789abcdef");
-    return (0);
-}
