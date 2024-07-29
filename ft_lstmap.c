08f291e2a668a728df921e73ab6f30861ecfb2a8
Anto <ant@onion.com>
2024-07-30 00:41:33 +0200

    limpiar y presentar

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: antofern <antofern@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 14:03:36 by antofern          #+#    #+#             */
-/*   Updated: 2024/07/29 14:51:12 by antofern         ###   ########.fr       */
+/*   Updated: 2024/07/30 00:21:52 by antofern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,34 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list *new;
-	t_list *current;
+	t_list	*new;
+	t_list	*current;
+	void	*new_content;
 
-	new = ft_lstnew((*f)(lst->content));
-	if (new->next == NULL)
+	if (lst == NULL || f == NULL || del == NULL)
+		return (NULL);
+	new_content = (*f)(lst->content);
+	new = ft_lstnew(new_content);
+	if (new == NULL)
 		return (NULL);
 	current = new;
-
-	while (lst->next != NULL)
+	lst = lst->next;
+	while (lst != NULL)
 	{
-		lst = lst->next;
-		current->next = ft_lstnew((*f)(lst->content));
+		if (lst->content == NULL)
+		{
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
+		new_content = (*f)(lst->content);
+		current->next = ft_lstnew(new_content);
 		if (current->next == NULL)
 		{
 			ft_lstclear(&new, del);
 			return (NULL);
 		}
 		current = current->next;
+		lst = lst->next;
 	}
 	return (new);
 }
--- list_test.c
+++ list_test.c
@@ -15,27 +15,45 @@ void char_free(void * pnt)
     free(pnt);
 }
 
+void putchar_custom(void *c)
+{
+	ft_putchar_fd(*(char *)c, 1);
+}
+
+void	*pnt_toupper(void *c)
+{
+	char *new;
+
+	new = malloc(sizeof(char));
+	if (*(char *)c > 96 && *(char *)c < 123)
+		*new = *(char *)c - 32;
+	return (new);
+}
+
 int main(void)
 {
     t_list	*start;
     t_list	*new_node;
-    char	*values = "abcdefgh";
+    t_list	*map;
+	char	*values = "abcdefghi";
     char	*pnt;
     int		i;
 
-    start = NULL;
 
-    for (i = 0; i < 8; i++)
+    start = NULL;
+	i = 0;
+    while ( i < 9)
     {
         pnt = (char *)malloc(sizeof(char));
         *pnt = values[i];
         new_node = ft_lstnew(pnt);
         ft_lstadd_back(&start, new_node);
+		i++;
     }
 
-    // Verificaciones
     t_list *current = start;
-    for (i = 7; i >= 0; i--)
+	i = 7;
+    while (i >= 0)
     {
         if (*(char *)(current->content) != values[7 - i])
         {
@@ -45,11 +63,16 @@ int main(void)
         }
 		printf("\n----%c---\n", *((char *)current->content));
         current = current->next;
+		i--;
     }
-	printf("\n----%c---\n", *((char *)start->next->next->next->content));
+	printf("esperado -d- (char *)start->next->next->next->content\n----%c---\n", *((char *)start->next->next->next->content));
     printf("Las funciones lst* funcionan correctamente.\n");
+    map = ft_lstmap(start, pnt_toupper, char_free);
+	ft_lstiter(start, putchar_custom);
+	ft_lstiter(map, putchar_custom);
+	ft_lstclear(&map, char_free);
+	ft_lstclear(&start, char_free);
 
-    // Liberar memoria
-    ft_lstclear(&start, char_free);
+
     return 0;
 }
