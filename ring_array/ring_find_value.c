2631b4f50c29ec012783537d03f1419111dc50ad
Antonio Fernandez Naharro <antofern@c2r12s1.42madrid.com>
2024-12-06 18:03:31 +0100

    ring_find_value.c line 27 now is more accurate

--- ring_array/ring_find_value.c
+++ ring_array/ring_find_value.c
@@ -6,7 +6,7 @@
 /*   By: antofern <antofern@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 10:30:10 by antofern          #+#    #+#             */
-/*   Updated: 2024/08/27 10:30:29 by antofern         ###   ########.fr       */
+/*   Updated: 2024/12/06 17:34:47 by antofern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ t_level ring_find_value(t_ring *ring, int value)
 		i++;
 	if (ring_get_level(ring, i) != value)
 		return (0);
-	if ((ring->fill / 2) > i)
+	if ((ring->fill / 2) >= i)
 		return (i);
 	else
 		return (-(ring->fill - i));
