1eec6bd1159f6126d2c17587c220b2c983178379
Antonio Fernandez Naharro <antofern@c2r19s4.42madrid.com>
2024-12-07 19:24:21 +0100

    modificado ring_find_value, para permitir level superiores al top, segun la logica circular

--- ring_array/ring_get_level.c
+++ ring_array/ring_get_level.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ring_get_level.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antofern <antofern@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: antofern <antofern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 12:56:45 by antofern          #+#    #+#             */
-/*   Updated: 2024/08/27 09:42:49 by antofern         ###   ########.fr       */
+/*   Updated: 2024/12/07 18:25:39 by antofern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,16 @@ int ring_get_level(const t_ring *ring, t_level level)
 {
 	int index;
 
-	if (level >= ring->fill || level <= -(ring->fill))
-		return (0);
-	if (level < 0) {
+	while (level >= ring->fill)
+		level = level - ring->fill;
+	while (level <= -(ring->fill))
+		level = level + ring->fill;
+	if (level < 0)
+	{
 		level = ring->fill + level;
 	}
 	
 	index = (level + ring->head) % ring->slots ;
-
+	
 	return (ring->buff[index]);
 }
